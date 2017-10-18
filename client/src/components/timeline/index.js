f0212f095ebdea307631990a9145265181515fe1
Lucas Polito <lucas.a.polito@gmail.com>
2017-10-18 14:58:09 -0400

    Configure css modules

---
+++ client/src/components/timeline/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './timeline.css'
+
+class Timeline extends React.Component {
+  render() {
+    return (
+      <div className={styles.test}>test</div>
+    );
+  }
+}
+
+export default Timeline;
