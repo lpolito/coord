1bddd07e35f1a6214a47db60aacc4d5d739f8d87
Lucas Polito <lucas.a.polito@gmail.com>
2017-11-06 17:03:39 -0500

    Fix coords db getters

--- server/routes/coords/coords.controller.js
+++ server/routes/coords/coords.controller.js
@@ -1,78 +1,44 @@
 const _ = require('lodash');
 const ytVideos = require('./../../apis/google/youtube').videos;
 const Coord = require('./../../models/coord');
+const Angle = require('./../../models/angle');
+const Coordinate = require('./../../models/coordinate');
 const timeUtils = require('./../../utils').time;
 
 const get = (req, res) => {
-  let coord;
-  new Promise((resolve) => {
-    // TODO if req.params.id there, get one, else get all
-
-    Coord.query()
-      .insertGraph({
-        title: 'First attempt at coordinations',
-        angles: [
-          {
-            author: 'YouTube Star 1',
-            coordinates: [{
-              ytId: 'BMTM0eTDYK8',
-              xCoord: 0
-            }]
-          },
-          {
-            author: 'Some guy',
-            coordinates: [{
-              ytId: 'BMTM0eTDYK8',
-              xCoord: 200
-            }]
-          }
-        ]
-      })
-      .then((coords) => {
-        // console log
-        console.log(coords);
-      })
-      .catch(err => console.log(err));
-
-    // fake data from db
-    coord = {
-      id: req.params.id,
-      title: 'First attempt at coordinations',
-      angles: [
-        {
-          id: 1,
-          author: 'YouTube Star 1', // replace with ytAuthorId
-          coordinates: [{
-            id: 1,
-            ytId: 'BMTM0eTDYK8', // video id
-            ytLength: null, // length of yt video in seconds
-            // ytStart: 0, // place to start yt video
-            xCoord: 0, // x position on timeline
-            selected: true // whether or not it's going to play
-          }]
-        }
-      ]
-    };
-
-    const videoPromises = [];
-    _.forEach(coord.angles, (angle) => {
-      _.forEach(angle.coordinates, (coordinate) => {
-        // populate youtube data from ytId
-        videoPromises.push(ytVideos.get(coordinate.ytId)
-          .then((video) => {
-            // TODO: parse what duration actually means (eg PT4M12S)
-            coordinate.ytLength = timeUtils.ytDurationToSeconds(video.contentDetails.duration);
-          }));
+  Coord
+    .query()
+    .findById(req.params.id)
+    .eager('angles.coordinates')
+    .omit(Angle, ['coordId', 'createdAt', 'updatedAt'])
+    .omit(Coordinate, ['angleId', 'createdAt', 'updatedAt'])
+    .then((coord) => {
+      const ytVideoPromises = [];
+      _.forEach(coord.angles, (angle) => {
+        _.forEach(angle.coordinates, (coordinate) => {
+          // populate youtube data from ytId
+          ytVideoPromises.push(ytVideos.get(coordinate.ytId)
+            .then((ytVideo) => {
+              // parse yt duration to seconds
+              coordinate.ytLength = timeUtils.ytDurationToSeconds(ytVideo.contentDetails.duration);
+            }));
+        });
       });
-    });
 
-    resolve(Promise.all(videoPromises));
-  })
-    .then(() => {
-      res.send(coord);
+      Promise.all(ytVideoPromises)
+        .then(() => {
+          res.send(coord);
+        });
     });
 };
 
+const getAll = (req, res) => {
+  Coord
+    .query()
+    .then(coords => res.send(coords));
+};
+
 module.exports = {
-  get
+  get,
+  getAll
 };
--- server/routes/coords/index.js
+++ server/routes/coords/index.js
@@ -3,7 +3,7 @@ const CoordsController = require('./coords.controller');
 
 const router = express.Router();
 
-router.get('/coords', CoordsController.get);
+router.get('/coords', CoordsController.getAll);
 router.get('/coords/:id', CoordsController.get);
 
 module.exports = router;
