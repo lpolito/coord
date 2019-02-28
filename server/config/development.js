7ee89bba507b2b7971ef7603a40cb417e69c06b8
Lucas Polito <41299650+lpolito@users.noreply.github.com>
2019-02-28 09:13:12 -0500

    Update development.js
    Remove AWS connection details. ¯\_(ツ)_/¯

--- server/config/development.js
+++ server/config/development.js
@@ -4,10 +4,10 @@ module.exports = {
   // postgres
   pg: {
     connection: {
-      host: 'coorddb.crewbgowqrjr.us-east-2.rds.amazonaws.com',
+      host: '---',
       database: 'coord',
       user: 'coordmaster',
-      password: '6cc1b607e57a513ed81712e84ac1edce4c0b03c9'
+      password: '---'
     }
   }
 };
